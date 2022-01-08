42c662ed506fd08416857ff76b944e332cd861b0
Khaled Adad <khaledadad@lewisu.edu>
2022-01-07 22:37:34 -0800

    added documentation to articl.js in routes folder

--- routes/articles.js
+++ routes/articles.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const Article = require('../models').Article;
+const Article = require('../models').Article; // include the article model, require the index file of the routers folder with const (don't have to specify index.js)
 
-/* Handler function to wrap each route. */
+// using .Article we are accessing the articles model specifically,
+// and all associated ORM methods such as find all, create, update, destroy
+
+// We can access each model that we define via a property that gets imported from the code in the models index.js file
+//
+
+/* Handler function to wrap each route. In other words, we are abstracting away the try catch for all of our calls by creating an asyncHandler function and using or implimenting it in all of our calls.*/
 function asyncHandler(cb) {
     return async(req, res, next) => {
         try {
             await cb(req, res, next)
         } catch (error) {
-            // Forward error to the global error handler
-            next(error);
+            // res.status(500).send(error) // this line/catch statement will send a 500 error or internal server error to the user if an exception is thrown in the try block, or in any of the handler functions           
+            next(error); // Forward error to the global error handler
         }
     }
 }
 
+/*
+The articles.js file contains several routes to handle HTTP requests, like POST and GET.
+
+The handler functions execute when a route matches the path defined.
+
+Since sequelize is promise-based, most of the handlers are going to perform asynchronous actions to make calls to the database and execute all the CRUD operations.
+
+NOTE: WE ARE STILL USING THE async/await syntax though for all asynchronous calls made w/sequelize
+
+And we want to use a try/catch statement in each handler to run the code that needs to be executed and catch any exceptions that are thrown. 
+*/
+
 /* GET articles listing. */
 router.get('/', asyncHandler(async(req, res) => {
-    const articles = await Article.findAll({ order: [
+    const articles = await Article.findAll({
+        order: [
             ["createdAt", "DESC"]
-        ] }); // display articles in descending order
+        ]
+    }); // display articles in descending order
     res.render("articles/index", { articles, title: "Sequelize-It!" });
 }));
 
